9bb6dac8b356ab72c737ec8a1a18216961f04774
Rémy Lavainne <rlavainne@antidot.net>
2023-06-19 10:15:20 +0200

    Add nvim-surround plugin

--- init.lua
+++ init.lua
@@ -106,6 +106,16 @@ require('lazy').setup({
       'rafamadriz/friendly-snippets',
     },
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
