f99a3ed74f82fd63d8ba9219cee5a52a830b8855
Rémy Lavainne <rlavainne@antidot.net>
2023-06-19 10:15:59 +0200

    Add keymap for gitsigns

--- init.lua
+++ init.lua
@@ -135,6 +135,10 @@ require('lazy').setup({
         vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        vim.keymap.set('n', '<leader>hs', require('gitsigns').stage_hunk,  {buffer = bufnr, desc = '[H]unk [S]tage' })
+        vim.keymap.set('n', '<leader>hr', require('gitsigns').reset_hunk,  {buffer = bufnr, desc = '[H]unk [R]eset' })
+        vim.keymap.set('n', '<leader>hb', function() require('gitsigns').blame_line{full=true} end, {buffer = bufnr, desc = 'Lines [B]lame' })
+        vim.keymap.set('n', '<leader>hd', require('gitsigns').diffthis,  {buffer = bufnr, desc = '[H]unk [D]iff' })
       end,
     },
   },
