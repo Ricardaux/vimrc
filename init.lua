454abb1c4946aa2d9bb0368c5c7e1c87c2485278
Rémy Lavainne <rlavainne@antidot.net>
2023-08-04 16:27:54 +0200

    Remove useless init.lua

--- init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require("plugins.init")
-require("setup.init")
-
-vim.keymap.set('n', 'gr', '<Cmd>lua require"nvim-treesitter.textobjects".go_to_previous_start()<CR>:lua require("telescope.builtin").lsp_references()<CR>', { silent = true })
