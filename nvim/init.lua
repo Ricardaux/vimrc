c18a782c754e342187c1d58111a8c8563e06936d
Rémy Lavainne <rlavainne@antidot.net>
2023-08-04 15:45:00 +0200

    Add local config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@ vim.g.maplocalleader = ' '
 
 require("plugins.init")
 require("setup.init")
+require("local.init")
 
 vim.keymap.set('n', 'gr', '<Cmd>lua require"nvim-treesitter.textobjects".go_to_previous_start()<CR>:lua require("telescope.builtin").lsp_references()<CR>', { silent = true })
