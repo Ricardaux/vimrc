8f69277a57952f022032c687f49b9e5c624ca7c5
Rémy Lavainne <rlavainne@antidot.net>
2023-08-28 09:11:45 +0200

    Fix step out dap keymap

--- nvim/lua/plugins/debug/dap.lua
+++ nvim/lua/plugins/debug/dap.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end, { desc = '[D]ebug [C]ontinue' })
 vim.keymap.set('n', '<leader>dn', function() require('dap').step_over() end, { desc = '[D]ebug Step [N]ext' })
-vim.keymap.set('n', '<leader>do', function() require('dap').step_over() end, { desc = '[D]ebug Step [O]ut' })
+vim.keymap.set('n', '<leader>do', function() require('dap').step_out() end, { desc = '[D]ebug Step [O]ut' })
 vim.keymap.set('n', '<leader>di', function() require('dap').step_into() end, { desc = '[D]ebug Step [I]nto' })
 vim.keymap.set('n', '<leader>dp', function() require('dap').step_back() end, { desc = '[D]ebug [P]revious Step' })
 vim.keymap.set('n', '<leader>db', function() require('dap').toggle_breakpoint() end, { desc = '[D]ebug Toggle [B]reakpoint' })
