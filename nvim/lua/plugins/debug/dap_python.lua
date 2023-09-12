9609728f74ae0251f0d9b4c659a4caac8de6db91
Rémy Lavainne <rlavainne@antidot.net>
2023-09-12 15:44:50 +0200

    Add remote debugger

--- nvim/lua/plugins/debug/dap_python.lua
+++ nvim/lua/plugins/debug/dap_python.lua
@@ -2,11 +2,9 @@ require('dap-python').test_runner = 'pytest'
 require('dap-python').setup('/Users/rlavainne/.pyenv/shims/python3')
 table.insert(require('dap').configurations.python, {
   type = 'python',
-  request = 'launch',
-  name = 'Run connector',
-  program = '${file}',
-  args = {'run'}
-  -- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+  request = 'attach',
+  name = 'Connector',
+  port = 1234
 })
 
 vim.keymap.set('n', '<leader>df', function() require('dap-python').test_class() end, { desc = '[D]ebug Class' })
