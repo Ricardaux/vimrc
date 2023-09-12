1f0d01f03f402c330788f24a12ad5d5e942a01d2
Rémy Lavainne <rlavainne@antidot.net>
2023-09-12 15:45:21 +0200

    Add formatter

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -2,6 +2,7 @@ require("null-ls").setup({
   sources = {
     require("null-ls").builtins.formatting.autopep8,
     require("null-ls").builtins.formatting.shfmt, -- shell script formatting
+    require("null-ls").builtins.formatting.prettier,
     -- require("null-ls").builtins.formatting.isort,
     require("null-ls").builtins.formatting.autoflake,
     require("null-ls").builtins.diagnostics.flake8
