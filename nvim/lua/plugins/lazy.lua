de329b9df3e294a3dda88d1166cde9bc85c2f2fc
Rémy Lavainne <rlavainne@antidot.net>
2023-09-13 13:57:20 +0200

    Add undotree

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -173,6 +173,8 @@ require('lazy').setup({
   'mfussenegger/nvim-dap',
   'mfussenegger/nvim-dap-python',
   'leoluz/nvim-dap-go',
-  'rcarriga/nvim-dap-ui'
+  'rcarriga/nvim-dap-ui',
 
+
+  'mbbill/undotree'
 }, {})
