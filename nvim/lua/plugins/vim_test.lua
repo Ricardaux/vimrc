b92d3539d0fdddb64349f18e0cad59184f945e8c
Rémy Lavainne <rlavainne@antidot.net>
2023-12-20 13:48:08 +0100

    Save before running test

--- nvim/lua/plugins/vim_test.lua
+++ nvim/lua/plugins/vim_test.lua
@@ -1,9 +1,16 @@
+local function saveBeforeCommand(command)
+  return function ()
+    vim.cmd(':w')
+    vim.cmd(command)
+  end
+end
+
 vim.g['test#strategy'] = 'dispatch'
 vim.keymap.set('n', '<leader>of', 'gf<C-W>o', { buffer = bufnr, desc = '[O]pen [F]ile from quickfixlist' })
-vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', { buffer = bufnr, desc = '[T]est [N]earest' })
-vim.keymap.set('n', '<leader>tc', ':TestClass<CR>', { buffer = bufnr, desc = '[T]est [C]lass' })
-vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { buffer = bufnr, desc = '[T]est [F]ile' })
-vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>', { buffer = bufnr, desc = '[T]est [S]uite' })
-vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { buffer = bufnr, desc = '[T]est [L]ast' })
-vim.keymap.set('n', '<leader>tv', ':TestVisit<CR>', { buffer = bufnr, desc = '[T]est [V]isit' })
+vim.keymap.set('n', '<leader>tn', saveBeforeCommand('TestNearest'), { buffer = bufnr, desc = '[T]est [N]earest' })
+vim.keymap.set('n', '<leader>tc', saveBeforeCommand('TestClass'), { buffer = bufnr, desc = '[T]est [C]lass' })
+vim.keymap.set('n', '<leader>tf', saveBeforeCommand('TestFile'), { buffer = bufnr, desc = '[T]est [F]ile' })
+vim.keymap.set('n', '<leader>ts', saveBeforeCommand('TestSuite'), { buffer = bufnr, desc = '[T]est [S]uite' })
+vim.keymap.set('n', '<leader>tl', saveBeforeCommand('TestLast'), { buffer = bufnr, desc = '[T]est [L]ast' })
+vim.keymap.set('n', '<leader>tv', saveBeforeCommand('TestVisit'), { buffer = bufnr, desc = '[T]est [V]isit' })
 
