730f6ee8c97fe93b2aaf5de2de0be9f01f461be2
Ricardau <ricardau@laposte.net>
2024-01-07 18:19:27 +0100

    Use create_autocmd to auto save file

--- nvim/lua/setup/autosave.lua
+++ nvim/lua/setup/autosave.lua
@@ -1,6 +1,8 @@
-vim.cmd([[
-  augroup auto_save
-    autocmd!
-    autocmd BufLeave * silent! write
-  augroup END
-]])
+vim.api.nvim_create_autocmd("TextChanged", {
+	pattern = { "*" },
+	callback = function()
+		if vim.api.nvim_buf_get_option(0, "modifiable") and vim.api.nvim_buf_get_option(0, "buftype") == "" then
+			vim.api.nvim_command("write")
+		end
+	end,
+})
