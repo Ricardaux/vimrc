070c5cb7ed6d1da3cd46bf94381abb3042e6a22d
Ricardau <ricardau@laposte.net>
2024-01-07 18:42:04 +0100

    Add cpp to conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,9 +1,9 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
-    python = { "autopep8" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "autopep8" },
+		cpp = { "clang_format" },
+	},
 })
 
-vim.keymap.set('n', '<leader>ff', require("conform").format, { desc =  '[F]ormat [F]ile' })
+vim.keymap.set("n", "<leader>ff", require("conform").format, { desc = "[F]ormat [F]ile" })
