8c752276f9ddec55e6d32cec57891937a7b993e1
Rémy Lavainne <rlavainne@antidot.net>
2024-01-24 09:13:16 +0100

    Add multicursors

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -171,6 +171,23 @@ require('lazy').setup({
   'leoluz/nvim-dap-go',
   'rcarriga/nvim-dap-ui',
 
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'smoka7/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+            {
+                mode = { 'v', 'n' },
+                '<Leader>m',
+                '<cmd>MCstart<cr>',
+                desc = 'Create a selection for selected text or word under the cursor',
+            },
+        },
+  },
 
   'mbbill/undotree'
 }, {})
