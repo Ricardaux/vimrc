20f774579b046ac1340334e4f0b74753203dd478
Rémy Lavainne <rlavainne@antidot.net>
2024-06-10 13:57:58 +0200

    Change dap ui configuration

--- nvim/lua/plugins/debug/dap_ui.lua
+++ nvim/lua/plugins/debug/dap_ui.lua
@@ -1,21 +1,28 @@
 require("dapui").setup({
-    layouts = { {
-        elements = {
-          {
-            id = "scopes",
-            size = 0.5
-          },
-          {
-            id = "watches",
-            size = 0.2
-          },
-          {
-            id = "stacks",
-            size = 0.3
-          },
-        },
-        position = "left",
-        size = 40
-      } }})
+  layouts = { {
+    elements = {
+      {
+        id = "scopes",
+        size = 0.5
+      },
+    },
+    position = "bottom",
+    size = 0.25
+  }, {
+    elements = {
+      {
+        id = "watches",
+        size = 0.2
+      },
+      {
+        id = "stacks",
+        size = 0.3
+      },
+    },
+    position = "left",
+    size = 40
+  }
+  }
+})
 vim.keymap.set('n', '<leader>du', function() require('dapui').open() end, { desc = '[D]ebug [U]i' })
 vim.keymap.set('v', '<leader>dw', require("dapui").eval, { desc = '[D]ebug [W]atch' })
