c05504e8a4537b1b2a318ab17613501598f749c8
Rémy Lavainne <rlavainne@antidot.net>
2024-09-30 14:52:41 +0200

    Add shortcut to go to underscore

--- nvim/lua/setup/shortcut.lua
+++ nvim/lua/setup/shortcut.lua
@@ -22,3 +22,4 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 -- vim.keymap.set('n', '<C-f>', function() vim.fn.search('\\u') end, { desc = 'Go to new Uppercase' })
+-- vim.keymap.set('n', '<C-b>', function() vim.fn.search('_') end, { desc = 'Go to new Underscore' })
