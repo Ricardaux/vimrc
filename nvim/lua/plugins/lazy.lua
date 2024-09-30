7c74d2c8a6b9c33f5cd4b404530c7970612575b0
Rémy Lavainne <rlavainne@antidot.net>
2024-09-30 14:39:03 +0200

    Remove rest configuration

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -202,11 +202,6 @@ require('lazy').setup({
   },
   {
     "rest-nvim/rest.nvim",
-    ft = "http",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-      require("rest-nvim").setup()
-    end,
   },
 
   'mbbill/undotree',
