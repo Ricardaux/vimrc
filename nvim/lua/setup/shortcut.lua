fffcda3b30af61463d956cc24f9b1257309f65ac
Rémy Lavainne <rlavainne@antidot.net>
2024-09-30 14:52:24 +0200

    Add shortcut to go to Uppercase

--- nvim/lua/setup/shortcut.lua
+++ nvim/lua/setup/shortcut.lua
@@ -21,3 +21,4 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- vim.keymap.set('n', '<C-f>', function() vim.fn.search('\\u') end, { desc = 'Go to new Uppercase' })
