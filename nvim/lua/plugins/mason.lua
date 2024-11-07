82da75ea538498b33100c740b33d6a49fef33e1f
Rémy Lavainne <rlavainne@antidot.net>
2024-11-07 15:27:45 +0100

    Remove include_declaration for lsp_references

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -17,7 +17,9 @@ local on_attach = function(_, bufnr)
   vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc =  '[C]ode [A]ction' })
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gr', function ()
+    require('telescope.builtin').lsp_references { include_declaration = false }
+  end, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
